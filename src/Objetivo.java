5130a68209ee9dbc71fa346882694d6e364fb303
Kevin Araya Reygada <kevxar@gmail.com>
2018-01-18 12:45:57 -0300

    Implementacion de clases
    Implementación de clase Objetivo
    Implementación de clase Mision (en proceso)

--- src/Objetivo.java
+++ src/Objetivo.java
@@ -4,5 +4,16 @@
  * @author Joaquin Solano
  */
 public class Objetivo {
-
+	private String nombre;
+	private String descripcion;
+	private int x;
+	private int y;
+	
+	public Objetivo(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.nombre = "Bomba";
+		this.descripcion = "Soy la bomba destructora, enemigos alejaos y temed!";
+	}
+	
 }
