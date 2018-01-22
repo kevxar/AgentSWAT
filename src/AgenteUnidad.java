9bef6c9e60a56afbf5177e3e97ca0940fd61f6fc
Baldo Morales Choque <32346079+BaldoMC@users.noreply.github.com>
2018-01-22 11:05:12 -0300

    Modificación clase AgenteUnidad

--- src/AgenteUnidad.java
+++ src/AgenteUnidad.java
@@ -1,10 +1,125 @@
+import java.io.IOException;
+
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
 
 /**
+ * Clase Agente de una Unidad, que permite ayudar al lider con revisar zonas de mapas
+ * e informar los estados.
  * @author Baldo Morales
  * @author Kevin Araya
  * @author Joaquin Solano
  */
 public class AgenteUnidad extends Agent {
-
+	// Variable nombre que contiene el nombre del agente.
+	private String nombre;
+	
+	// Matriz perimetro que contiene un sector del mapa original.
+	private int[][] perimetro;
+	
+	// Variable estado que contiene los 3 estados del agente "despejado", "encontrado" y "desactivado".
+	private String estado;
+	
+	//Variables que guardan coordenadas de la bomba
+	private int bombaX;
+	private int bombaY;
+	
+	/**
+	 * Setup que inicializa el agente Unidad.
+	 */
+	protected void setup() {
+		nombre = this.getLocalName();
+		//Se añade el servicio de disponibilidad de Unidad SWAT
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("unidad-swat");
+		sd.setName("Unidad-SWAT-Disponible");
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		}catch(FIPAException fe) {
+			fe.printStackTrace();
+		}
+		System.out.println("Unidad: "+nombre+" disponible para la mision.");
+	}
+	/**
+	 * Cuando el Agente manda un mensaje al irse
+	 */
+	protected void takeDown() {
+		System.out.println("Unidad: "+nombre+" termina su servicio.");
+	}
+	/**
+	 * Clase Revisar Perimetro que es un comportamiento
+	 * ciclico que espera una solicitud del lider,
+	 * revisa en una matriz si existe un objeto dentro de las casillas,
+	 * cuando termine la evaluación procedera a notificar el estado.
+	 * @author Baldo Morales
+	 *
+	 */
+	private class revisarPerimetro extends CyclicBehaviour{
+		public void action() {
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
+			ACLMessage msg = myAgent.receive(mt);
+			// Se verifica si el mensaje esta vacio.
+			if(msg != null) {
+				try {
+					perimetro = (int[][]) msg.getContentObject();
+				} catch (UnreadableException e) {
+					e.printStackTrace();
+				}
+				// Se inicia el estado como despejado.
+				estado = "despejado";
+				
+				for(int i = 0; i < perimetro.length ; i++) {
+					for(int j = 0; j < perimetro[0].length; j++) {
+						// En caso de encontrar un "1" dentro de la matriz, se cambia el estado a "encontrado" y se sale de inmediato.
+						if(perimetro[i][j] == 1) {
+							estado = "encontrado";
+							bombaX = i;
+							bombaY = j;
+							System.out.println("Agente "+nombre+" reviso la zona ("+i+","+j+") y encontro la bomba");
+							
+							break;
+						}
+						System.out.println("Agente "+nombre+" reviso la zona ("+i+","+j+") y esta "+estado);
+					}
+					if(estado == "encontrado") break;
+				}
+				
+				// Se inicia el comportamienteo Noticar Estado.
+				addBehaviour(new notificarEstado());
+				
+			}else {
+				block();
+			}
+			
+		}
+	} // Fin de la clase Revisar Perimetro
+	
+	/**
+	 * Clase Notificar Estado que informa al lider con el String estado,
+	 * solo se realiza una vez cada vez que se revisa el perimetro.
+	 * @author Baldo Morales
+	 *
+	 */
+	private class notificarEstado extends OneShotBehaviour{ 
+		public void action() {
+			System.out.println("Se procede a notificar");
+			ACLMessage inform = new ACLMessage(ACLMessage.INFORM);
+			inform.addReceiver(new AID("Lider", AID.ISLOCALNAME));
+			inform.setContent(estado);
+			send(inform);
+		}
+	} // Fin de la clase Notificar Estado
+	
+	
 }
